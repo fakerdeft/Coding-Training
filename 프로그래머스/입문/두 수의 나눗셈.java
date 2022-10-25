b6a0d745dbf31dbc466236274e24db0867861d83
아맞다우산 <98208452+fakerdeft@users.noreply.github.com>
2022-10-25 23:42:54 +0900

    Create 두 수의 나눗셈.java

---
+++ 프로그래머스/입문/두 수의 나눗셈.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        int answer = 0;
+        double re = ((double)num1/num2)*1000;
+        
+        answer = (int)re;
+        
+        return answer;
+    }
+}
