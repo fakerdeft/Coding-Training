ee86c1042a42a62327ff88879b4b8e777850f8f7
아맞다우산 <98208452+fakerdeft@users.noreply.github.com>
2022-10-25 23:51:18 +0900

    Add files via upload

---
+++ 백준/2739번-구구단.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+	static Scanner sc=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		int a=sc.nextInt();
+		
+		for(int i=1; i<10;i++) {
+			System.out.println(a+" * "+i+" = "+a*i);
+		}     
+    }      
+}
